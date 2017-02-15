f580da5a7a83e5e105c0b55691a6c6663e4352d9
Ömer Sinan Ağacan <omeragacan@gmail.com>
2017-02-15 14:45:13 +0300

    add hs.nix

---
+++ hs.nix
@@ -0,0 +1,13 @@
+{ mkDerivation, base, lifted-base, monad-control, stdenv, stm
+, transformers-base
+}:
+mkDerivation {
+  pname = "privileged-concurrency";
+  version = "0.4";
+  src = ./.;
+  libraryHaskellDepends = [
+    base lifted-base monad-control stm transformers-base
+  ];
+  description = "Provides privilege separated versions of the concurrency primitives";
+  license = stdenv.lib.licenses.bsd3;
+}
