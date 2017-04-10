ccae4e774c70e6d63e7e65b0ae5e674aeb1d6e8c
Ömer Sinan Ağacan <omeragacan@gmail.com>
2017-04-10 09:19:23 +0300

    bump hs.nix

--- hs.nix
+++ hs.nix
@@ -1,12 +1,12 @@
-{ mkDerivation, base, lifted-base, monad-control, stdenv, stm
-, transformers-base
+{ mkDerivation, base, contravariant, lifted-base, monad-control
+, stdenv, stm, transformers-base
 }:
 mkDerivation {
   pname = "privileged-concurrency";
-  version = "0.5";
-  src = ./.;
+  version = "0.6";
+  sha256 = "0ns24fvxjdjlhqb0f9fh73r6q8al9ixi197nc30g5b2b7csnixv7";
   libraryHaskellDepends = [
-    base lifted-base monad-control stm transformers-base
+    base contravariant lifted-base monad-control stm transformers-base
   ];
   description = "Provides privilege separated versions of the concurrency primitives";
   license = stdenv.lib.licenses.bsd3;
